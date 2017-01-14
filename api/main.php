f4f9c50bd11a4aff538052101967644fd7f818c0
flojat <f.jaton@universallink>
2017-01-14 01:06:11 +0100

    route /stats/week  modified

--- api/main.php
+++ api/main.php
@@ -21,43 +21,67 @@ $container['db'] = function ($c) {
 $app->get('/stats/week', function (Request $request, Response $response) {
     
 /*OPTIONAL;
-of_category_id Array of int(11)
-project_id int(11)
-search_text varchar(255)
-start_date date
-end_date date
-user_id int(11) (just if admin!)"	"get List of all Entries of that user within actual Week(7days)
-is used to show worked time this week"
-
-
-1) Liste der Projekte in der aktuellen Woche:
-
-SELECT  p.id 
-from entries e, projects p
-where `user_id` =5  AND e.project_id = p.id AND DATE_FORMAT(NOW(),"%U")=DATE_FORMAT(start,"%U")
-group by p.name 
-
-('11','10','9')
-
-2) Liste der Tage mit Einträgen in der aktuellen Woche:
-
-('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+    of_category_id Array of int(11)
+    project_id int(11)
+    search_text varchar(255)
+    start_date date
+    end_date date
+    user_id int(11) (just if admin!)"	"get List of all Entries of that user within actual Week(7days)
+    is used to show worked time this week"
 */
 
     
     
     $jsonData="";
+    
     $dayOfWeek =  array('Mon','Tue','Wed','Thu','Fri','Sat','Sun');
     $projectsWithProgress =  array(9,10,11);
     $user_id=3;
     
+    //$arrayDataTable =  array('WEEKDAY','Mon','Tue','Wed','Thu','Fri','Sat','Sun');
+    $projectsWithProgress  = [
+    '9' => ["Proj 1"],
+    '10' => ["Proj 2"],
+    '11' => ["Proj 3"],
+    ];
     
-    foreach ($dayOfWeek as &$day) {
-        foreach ($projectsWithProgress as &$proj) {
-        $sqlquery="SELECT sum(round((TIME_TO_SEC(TIMEDIFF(`stop`,`start`))/60/60),2)) from entries e where `user_id`=$user_id AND DATE_FORMAT(`start`,'%a')='$day' AND e.project_id=$proj";
-        $jsonData .= $sqlquery." ";
-        }
+    $jsonData .="['WEEKDAY'";
+    foreach ($projectsWithProgress as &$proj) {
+      $jsonData .= ",'".$proj[0]."'";
     }
+    $jsonData .= "]";
+    
+/*    
+  [
+      ['WEEKDAY', 'Log-Box, Universal Link',
+          'Redesign Web, Swisscom AG',
+          'Mobile App, Kuster AG',
+          'Req. Analyse, FHNW',
+          'Int. Buchhaltung',
+          'Int. Diversa'],
+      ['Mo.', 2, 1, 3, 1, 0, 0],
+      ['Di.', 3, 0.5, 1, 1, 0, 0],
+      ['Mi.', 0.5, 4, 6, 0, 0, 0],
+      ['Do.', 1, 0.25, 1, 2, 0, 0],
+      ['Fr.', 0.5, 1, 2, 1, 0, 1],
+      ['Sa.', 0, 0, 0, 0, 3, 0.5,]
+  ]
+*/    
+
+    foreach ($dayOfWeek as &$day) {
+        $jsonData.= ",['$day'"; 
+        foreach ($projectsWithProgress as $projId => $projName) {
+            $sqlquery="SELECT sum(round((TIME_TO_SEC(TIMEDIFF(`stop`,`start`))/60/60),2)) h from entries e WHERE `user_id`=$user_id AND DATE_FORMAT(`start`,'%a')='$day' AND e.project_id=".$projId;
+            $stmt = $this->db->query($sqlquery);    
+            while($row = $stmt->fetch()) {
+              $jsonData.= ",".floatval (($row['h'] != "") ? $row['h'] : 0 ); 
+            }
+            $stmt = null;
+        }
+        $jsonData.= "]"; 
+    }
+    
+    //$jsonData .= json_encode($arrayDataTable); 
 
     //$stmt = $this->db->query($sqlquery); 
     //while($row = $stmt->fetch()) {
@@ -151,6 +175,10 @@ $app->post('/project/entry', function (Request $request, Response $response) {
       $logbox_mac = filter_var($bodyData['logbox_mac'], FILTER_SANITIZE_STRING);
       $project_id = (int)filter_var($bodyData['project_id'], FILTER_SANITIZE_STRING);
       $go_to_standby = filter_var($bodyData['go_to_standby'], FILTER_SANITIZE_STRING);
+      
+      $user_id =  3;
+      $logbox_mac = "138.174.117.190";
+      
       
       $logbox_ip = "unset";
       if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] !=""){
